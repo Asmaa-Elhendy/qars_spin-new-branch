08c3659beedd77b7c15cec269721cc0f1912aca6
Asmaa Elhendy <asmaaelhendy7055@gmail.com>
2025-08-31 15:30:36 +0300

    last update

--- lib/showroom/pages/show_room_detail.dart
+++ lib/showroom/pages/show_room_detail.dart
@@ -29,35 +29,49 @@ class CarDealerScreen extends StatelessWidget {
           // ===== 3. Action Buttons =====
           ActionButtons(),
 
-          SizedBox(height: 16),
+          SizedBox(height: height*.01),
 
           // ===== 4. Tabs =====
           DealerTabs(),
           Center(
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: width * .01,
-                vertical: height * .006,
-              ),
-              height: height * .08,
-              width: width * .46,
-              child: ElevatedButton.icon(
-                onPressed: () {},
-                icon: Icon(Icons.directions_car, color: Colors.black),
-                label: Text(
-                  "Showroom Cars",
-                  style: TextStyle(color: Colors.black),
+            child: InkWell(
+              onTap: () {
+                // 👈 هنا تحط الأكشن اللي عايزه
+              },
+              borderRadius: BorderRadius.circular(10), // عشان يعمل ripple بنفس الشكل
+              child: Container(
+                height: height * .06,
+                width: width * .46,
+                margin: EdgeInsets.symmetric(
+                  vertical: height * .006,
                 ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor:Color(0xFFF6C42D),
-                  padding: EdgeInsets.symmetric(vertical: 14),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+                padding: EdgeInsets.symmetric(
+                  horizontal: width * .01,
+                  vertical: height * .006,
+                ),
+                decoration: BoxDecoration(
+                  color: const Color(0xFFF6C42D), // الخلفية الصفراء
+                  borderRadius: BorderRadius.circular(10), // الزوايا
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(Icons.directions_car, color: Colors.black),
+                    const SizedBox(width: 8),
+                    Text(
+                      "Showroom Cars",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: width * .038,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-          ),
+          )
+
         ],
       ),
     );
